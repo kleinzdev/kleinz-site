bc220ddef8fd5430cc1f70d90caef9aab756e639
kleinzdev <miguel.buccat12@gmail.com>
2023-12-13 15:33:20 +0800

    start design thingy

---
+++ components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+const socialDropdown = ref(false);
+</script>
+
+<template>
+  <navbar class="flex flex-row px-4 lg:px-52 py-2 justify-between">
+    <div class="flex flex-row gap-2 items-center">
+      <img class="rounded-full" width="55" height="55" src="/kaguya.png" />
+      <a class="text-sm text-white hover:bg-[#2C2C2C] transition px-2 py-2 rounded-lg font-bold" href="#">Home</a>
+      <a class="text-sm text-white hover:bg-[#2C2C2C] transition px-2 py-2 rounded-lg font-bold" href="#">About</a>
+      <a class="text-sm text-white hover:bg-[#2C2C2C] transition px-2 py-2 rounded-lg font-bold" href="#">Projects</a>
+    </div>
+    <div class="flex flex-row gap-2 items-center">
+      <a class="text-sm text-white bg-[#3498DB] hover:bg-[#2F87BB] active:[#2683B3] transition px-2 py-2 rounded-lg font-bold" href="#">Blog</a>
+    </div>
+  </navbar>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="px-4 lg:px-52">
+    <div class="flex flex-col justify-center min-h-[400px]">
+      <h1 class="text-4xl text-white font-extrabold">Hi! I'm Kleinz</h1>
+      <h1 class="text-xl text-white">🇵🇭 Filipino | Full-stack web developer</h1>
+      <div class="flex flex-row flex-wrap mt-2 gap-2">
+        <a class="text-white bg-[#4CAF50] hover:bg-[#45A850] active:[#409846] transition px-4 py-2 rounded-lg font-bold" href="#">Learn more about me</a>
+        <a class="text-white bg-[#3498DB] hover:bg-[#2F87BB] active:[#2683B3] transition px-4 py-2 rounded-lg font-bold" href="#">See my work</a>
+      </div>
+    </div>
+  </div>
+</template>
